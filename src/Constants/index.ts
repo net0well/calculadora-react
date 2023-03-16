1ff4000dd44abd076e8fd4014f2e52b445eddae8
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2023-03-16 09:22:19 -0300

    Add files via upload

---
+++ src/Constants/index.ts
@@ -0,0 +1,16 @@
+
+interface buttonsConstType {
+  text: string
+  variation?: 0 | 1
+}
+
+export const buttonsConst: buttonsConstType[] = [
+  { text: '7' }, { text: '8' }, { text: '9' },
+  { text: 'Del', variation: 1 },
+  { text: '4' },{ text: '5' }, { text: '6' },
+  { text: '+' },{ text: '1' }, { text: '2' },
+  { text: '3' },{ text: '-' }, { text: '.' },
+  { text: '0' },{ text: '/' }, { text: 'x' },
+  { text: 'Reset', variation: 1 },
+  { text: '=', variation: 0 }
+]
